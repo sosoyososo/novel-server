d9ee61ab3c7812033eddfa3b58401ee604248472
karsa <wanghongtao114@gmail.com>
2020-06-04 14:26:13 +0800

    fix

--- NovelSpider/spider_test.go
+++ NovelSpider/spider_test.go
@@ -1,7 +1,16 @@
 package NovelSpider
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSpider(t *testing.T) {
+}
 
+func TestSpiderConf(t *testing.T) {
+	for _, confRef := range confMap {
+		conf := *confRef
+		fmt.Println(conf.IsSummaryPage("/book/18006/"))
+	}
 }
